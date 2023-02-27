15b41dfd4dc2a1d993af76edcafe68f4181c2544
RishiMullangi <71631240+RishiMullangi@users.noreply.github.com>
2023-02-27 11:57:44 -0500

    Minimax and failing eval

--- proj2_starter_code/multiAgents.py
+++ proj2_starter_code/multiAgents.py
@@ -45,10 +45,11 @@ class ReflexAgent(Agent):
         scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
         bestScore = max(scores)
         bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        print(bestIndices)
         chosenIndex = random.choice(bestIndices) # Pick randomly among the best
 
         "Add more of your code here if you want to"
-
+        print(legalMoves[chosenIndex])
         return legalMoves[chosenIndex]
 
     def evaluationFunction(self, currentGameState, action):
@@ -79,6 +80,7 @@ class ReflexAgent(Agent):
         minGhostDist = 0
 
         foodDist = [manhattanDistance(newPos, food) for food in list(newFood)]
+        # print(foodDist)
         if len(foodDist) > 0:
             minFoodDist = min(foodDist)
         else:
@@ -89,10 +91,20 @@ class ReflexAgent(Agent):
         minGhostDist = min(ghostDist)
 
         scaredTime = sum(newScaredTimes)
-
-        scale = [15, -10, -20, 10]
-        factors = [minFoodDist, foodLeft, minGhostDist, scaredTime]
-        score = sum([w * f for w, f in zip(scale, factors)])
+        # print(successorGameState.getScore())
+        score = 0
+        score -= minFoodDist
+        score -= foodLeft*15
+        if minGhostDist <=5:
+            score -= (5 - minGhostDist) * 500 
+        score += successorGameState.getScore()
+        
+        
+        # print("Min ghost dist is" , minGhostDist)
+        # print("Min Food dist is", minFoodDist)
+        if action == 'Stop':
+            score = -9999
+        print("Score is:" , score)
         return score
         "*** YOUR CODE HERE ***"
         
@@ -130,35 +142,93 @@ class MultiAgentSearchAgent(Agent):
         self.depth = int(depth)
 
 class MinimaxAgent(MultiAgentSearchAgent):
-    """
-    Your minimax agent (question 2)
-    """
+#     """
+#     Your minimax agent (question 2)
+#     """
+    
+
+    def minimax(self, gameState, agentIndex, depth):
+        if depth == 0 or gameState.isWin() or gameState.isLose():
+            option = Directions.STOP, self.evaluationFunction(gameState)
+            #return option
+        elif agentIndex == 0:
+            agentIndex += 1
+            option = self.maximizer(gameState, agentIndex, depth)
+        else:
+            if agentIndex == gameState.getNumAgents() -1:
+                agentIndex = 0
+                depth = depth - 1
+            else:
+                agentIndex += 1
+            option = self.minimizer(gameState, agentIndex, depth)
+            
+        return option
+
+    def maximizer(self, gameState, agentIndex, depth):
+        if gameState.isWin() or gameState.isLose():
+            return Directions.STOP, self.evaluationFunction(gameState)
+        maximum = -999999999999999999
+        action_legal = gameState.getLegalActions(0)
+        for action in action_legal:
+            temp_list = self.minimax(gameState.generateSuccessor(0, action), agentIndex, depth)
+            temp_score = temp_list[1]
+            if temp_score > maximum:
+                maximum = temp_score
+                curr_action = action
+        return_tup = (curr_action, maximum)
+        return return_tup
+    
+    def minimizer(self, gameState, agentIndex, depth):
+        
+        if gameState.isWin() or gameState.isLose():
+            return Directions.STOP, self.evaluationFunction(gameState)
+        if agentIndex == 0:
+            temp_index = gameState.getNumAgents()-1
+        else:
+            temp_index = agentIndex-1
+        minimum = 999999999999999
+        action_legal = gameState.getLegalActions(temp_index)
+        # for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
+        #     x,y = state[0]
+        for action in action_legal:
+            temp_list = self.minimax(gameState.generateSuccessor(temp_index, action), agentIndex, depth)
+            temp_score = temp_list[1]
+            if temp_score < minimum:
+                minimum =  temp_score
+                curr_action = action
+        return_tup = (curr_action, minimum)
+        return return_tup
+
 
     def getAction(self, gameState):
-        """
-        Returns the minimax action from the current gameState using self.depth
-        and self.evaluationFunction.
+#         """
+#         Returns the minimax action from the current gameState using self.depth
+#         and self.evaluationFunction.
 
-        Here are some method calls that might be useful when implementing minimax.
+#         Here are some method calls that might be useful when implementing minimax.
 
-        gameState.getLegalActions(agentIndex):
-        Returns a list of legal actions for an agent
-        agentIndex=0 means Pacman, ghosts are >= 1
+#         gameState.getLegalActions(agentIndex):
+#         Returns a list of legal actions for an agent
+#         agentIndex=0 means Pacman, ghosts are >= 1
 
-        gameState.generateSuccessor(agentIndex, action):
-        Returns the successor game state after an agent takes an action
+#         gameState.generateSuccessor(agentIndex, action):
+#         Returns the successor game state after an agent takes an action
 
-        gameState.getNumAgents():
-        Returns the total number of agents in the game
+#         gameState.getNumAgents():
+#         Returns the total number of agents in the game
 
-        gameState.isWin():
-        Returns whether or not the game state is a winning state
+#         gameState.isWin():
+#         Returns whether or not the game state is a winning state
 
-        gameState.isLose():
-        Returns whether or not the game state is a losing state
-        """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+#         gameState.isLose():
+#         Returns whether or not the game state is a losing state
+#         """
+#         "*** YOUR CODE HERE ***"
+    
+        temp_list = self.minimax(gameState, 0, self.depth)
+        # print(temp_list[0])
+        return temp_list[0]
+    
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
@@ -170,7 +240,7 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return Directions.EAST()
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
